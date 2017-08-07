97b359c8d40deb8168fd0f023c1e002244afa97e
YujiSoftware <yuji.software+github@gmail.com>
2017-08-08 01:48:25 +0900

    Initial commit

---
+++ tools/src/main/java/jp/yujisoftware/MagazineDatabase/MagazineDatabaseApplication.java
@@ -0,0 +1,12 @@
+package jp.yujisoftware.MagazineDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineDatabaseApplication.class, args);
+	}
+}
---
+++ tools/src/test/java/jp/yujisoftware/MagazineDatabase/MagazineDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.yujisoftware.MagazineDatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MagazineDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
