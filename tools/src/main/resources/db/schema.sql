30663d3846cf7863337589d819f8c10f77350f42
YujiSoftware <yuji.software+github@gmail.com>
2017-08-11 23:49:07 +0900

    Added database shcema (WIP)

---
+++ tools/src/main/resources/db/schema.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE artist (
+  artist_id serial PRIMARY KEY,
+  name text NOT NULL
+);
+
+CREATE TABLE manga (
+  manga_id serial PRIMARY KEY,
+  title text NOT NULL,
+  main_artist_id integer NOT NULL
+);
+
+CREATE TABLE magazine (
+  magazine_id serial PRIMARY KEY,
+  year integer NOT NULL,
+  number integer[] NOT NULL
+);
