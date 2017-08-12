d64d29c028261e041db1a707748ccf7b9a97bec7
YujiSoftware <yuji.software+github@gmail.com>
2017-08-12 20:55:08 +0900

    Added database schema (WIP)

--- tools/src/main/resources/db/schema.sql
+++ tools/src/main/resources/db/schema.sql
@@ -1,16 +1,23 @@
-﻿CREATE TABLE artist (
+﻿CREATE TABLE artists (
   artist_id serial PRIMARY KEY,
   name text NOT NULL
 );
 
-CREATE TABLE manga (
+CREATE TABLE mangas (
   manga_id serial PRIMARY KEY,
   title text NOT NULL,
   main_artist_id integer NOT NULL
 );
 
-CREATE TABLE magazine (
+CREATE TABLE magazines (
   magazine_id serial PRIMARY KEY,
   year integer NOT NULL,
   number integer[] NOT NULL
+);
+
+CREATE TABLE serials (
+  magazine_id integer NOT NULL,
+  manga_id integer NOT NULL,
+  "order" integer NOT NULL,
+  CONSTRAINT serials_pkey PRIMARY KEY (magazine_id, manga_id, "order")
 );
